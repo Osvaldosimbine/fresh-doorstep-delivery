ac46ae430389addea69c679c2e401af99ecd8dd9
Osvaldo Simbine <osvaldoosimbine@gmail.com>
2026-04-26 18:10:45 +0200

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/fresh-doorstep-delivery/sw.js', { scope: '/fresh-doorstep-delivery/' })})}
